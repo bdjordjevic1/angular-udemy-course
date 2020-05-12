1137a7f0463a5d4f1dbba1e7f2c8d065d1c04e38
Boban <b.djordjevic@msgnetconomy.net>
2020-05-12 08:11:37 +0200

    NOTASK: Working with dynamic components

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,23 +1,28 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {AuthResponseData, AuthService} from './auth.service';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {Router} from '@angular/router';
+import {AlertComponent} from '../shared/alert/alert.component';
+import {PlaceholderDirective} from '../shared/placeholder/placeholder.directive';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
 
   isLoginMode = true;
   authForm: FormGroup;
   isLoading = false;
   error: string = null;
+  @ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective;
+  subscription: Subscription;
 
   constructor(private authService: AuthService,
-              private router: Router) {
+              private router: Router,
+              private componentFactoryResolver: ComponentFactoryResolver) {
   }
 
   ngOnInit() {
@@ -50,10 +55,30 @@ export class AuthComponent implements OnInit {
       this.router.navigate(['/recipes']);
     }, errorMessage => {
       this.error = errorMessage;
+      this.showErrorAlert(errorMessage)
       this.isLoading = false;
     });
 
     this.authForm.reset();
   }
 
+  onHandleError() {
+    this.error = null;
+  }
+
+  private showErrorAlert(message: string) {
+    const alertComponentFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    const componentRef = this.alertHost.viewContainerRef.createComponent(alertComponentFactory);
+    componentRef.instance.message = message;
+    this.subscription = componentRef.instance.close.subscribe(() => {
+      this.subscription.unsubscribe();
+      this.alertHost.viewContainerRef.clear();
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
